ce7faab1c1943d7a46f4c97944f13d463688cf86
Emmanuele-Iannucci <emmanueleiannucci@gmail.com>
2025-07-16 19:56:50 +0200

    rename_repo_paliedispari

---
+++ script.js
@@ -0,0 +1,14 @@
+// Chiediamo all’utente di scegliere "pari" o "dispari".
+
+// Chiediamo all’utente di inserire un numero da 1 a 5.
+
+// Generiamo un numero casuale da 1 a 5 per il computer (con una funzione).
+
+// Sommiamo il numero dell’utente con quello del computer.
+
+// Creiamo una funzione che ci dica se la somma è pari o dispari.
+
+// Se la scelta dell’utente corrisponde al risultato, ha vinto. Altrimenti, ha perso.
+
+// Stampiamo tutti i passaggi in console.log() per capire cosa succede.
+
