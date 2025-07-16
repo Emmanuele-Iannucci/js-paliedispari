725a830ee9bcb554bdfd2960805d561b0c665f2c
Emmanuele-Iannucci <emmanueleiannucci@gmail.com>
2025-07-16 18:28:55 +0200

    add_steps_first_step

---
+++ main.js
@@ -0,0 +1,19 @@
+// STEPS: 
+ 
+// 1. Chiediamo all’utente una parola.
+
+// 2. Creiamo una funzione chiamata wordPalindroma.
+
+// 3. La funzione riceve una parola come parametro.
+
+// 4. All’interno della funzione confrontiamo la parola originale con la sua versione al contrario.
+
+// 5. SE sono uguali: La parola è palindroma. ALTRIMENTI non lo è. 
+
+// 6. Stampiamo il risultato in console 
+
+// -----------------
+
+// 1. Chiediamo all'utente di inserire una parola (In questo caso utilizziamo la parola "otto")
+
+const userWord = prompt ("Inserisci una parola per verificare se è palindroma");
